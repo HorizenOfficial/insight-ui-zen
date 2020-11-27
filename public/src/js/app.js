4d0a083bad107fee468bd46baddca2f6c915041f
cronicc <cronic@zensystem.io>
2020-11-27 18:25:06 +0000

    Change default locale to cn

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -3,7 +3,7 @@
 var testnet = false;
 var netSymbol = testnet ? 'TAZ' : 'zen';
 
-var defaultLanguage = localStorage.getItem('insight-language') || 'en';
+var defaultLanguage = localStorage.getItem('insight-language') || 'cn';
 var defaultCurrency = localStorage.getItem('insight-currency') || netSymbol;
 
 angular.module('insight',[
